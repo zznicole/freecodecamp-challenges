29f258235eafbd49a67bf2c68b57c0dfb993da06
Nicole Zheng <zznicole@hotmail.com>
2020-06-13 01:28:08 +0200

    regex challenges 18th-21st

--- src/script.js
+++ src/script.js
@@ -1,3 +1,25 @@
+//ES6: Use getters and setters to Control Access to an Object
+// class Thermostat {
+//   constructor(emp) {
+//     this._Temp = 5/9 * (fahrenheitTemp -32)
+//   }
+//   get temperature() {
+//     return this._Temp;
+//   }
+
+//   set temperature(updatedTemp){
+//     this.temp = updatedTemp;
+    
+//   }
+//  }
+
+//  const thermos = new Thermostat(76);
+//  let temp = thermos.temperature;
+//  thermos.temperature = 26;
+//  temp = thermos.temperature;
+
+
+
 // Introduction to the Regular Expression Challenges
 // Using the Test Method
 // Match Literal String
@@ -17,7 +39,7 @@
 // let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 // let vowelRegex = /[aeiou]/gi;
 // let result = quoteSample.match(vowelRegex);
-// console.log(result;)
+// console.log(result);
 
 //Regular Expressions: Match Letters of the Alphabet
 
@@ -74,7 +96,32 @@
 
 // Regular Expressions: Match Ending String Patterns
 
-let caboose = " The last car on a train is the caboose";
-let lastRegex = /caboose$/;
-let result = lastRegex.test(caboose);
-console.log(result);
+// let caboose = " The last car on a train is the caboose";
+// let lastRegex = /caboose$/;
+// let result = lastRegex.test(caboose);
+// console.log(result);
+
+//Regular Expressions: Match all Letters and Numbers
+// let quoteSample = "The five boxig wizards jump quickly."
+// let alphabetRegexV2 = /\w/g;
+// let result = quoteSample.match(alphabetRegexV2).length;
+
+//Regular Expressions: Match Everything But Letters and Numbers
+
+// let quoteSample = "THe five boxing wizards jump quickly.";
+// let nonAlphabetRegex = /\W/g;
+// let result = quoteSample.match(nonAlphabetRegex).length;
+
+//Regular Expressions: Match All Numbers
+
+// let movieName = "2001: A Space Odyssey";
+// let numRegex = /\d/g;
+// let result = matchName. match(numRegex).length;
+
+//Regular Expressions: Match All Non-Numbers
+
+// let movieName = "2001: A Space Odyssey";
+// let noNumRegex = /\D/g;
+// let result = movieName.match(noNumRegex).length;
+
+//Regular Expressions: Restrict Possible Usernames
