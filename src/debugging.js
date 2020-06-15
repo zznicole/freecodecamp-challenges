514a14fdfdad1b9d5b2cc991d5383f3a8ebd7ba6
Nicole Zheng <zznicole@hotmail.com>
2020-06-15 15:47:55 +0200

    debugging 1-6

---
+++ src/debugging.js
@@ -0,0 +1,40 @@
+//1. Debugging: Use the JavaScript Console to Check the Value of a Variable
+
+// let a = 5;
+// let b = 1;
+// a++;
+// console.log(a);
+// let sumAB = a + b;
+// console.log(sumAB);
+
+//2. Debugging: Understanding the Differences between the freeCodeCamp and Browser Console
+
+// let output = "Get this to log once in the freeCodeCamp console and twice in the browser console";
+// console.log(output);
+// console.clear();
+
+//3. Debugging: Use typeof to check the Type of a Variable
+
+// let seven = 7;
+// let three = "3";
+// console.log(seven + three);
+// console.log(typeof seven);
+// console.log(typeof three);
+
+//4. Debugging: Catching Misspelled Variable and Function Names
+
+// let receivables = 10;
+// let payables = 8;
+// let netWorkingCapital = receivables - payables;
+// console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//5. Debugging: Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+// let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce((previous, current) => previous + current);
+// console.log(`Sum of array values is: ${arraySum}`);
+
+//6. Debugging: Catch Mixed Usage of Single and Double Quotes
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
