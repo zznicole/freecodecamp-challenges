ba029ee2786bfa7ac3b2ba82823b4cb16eac2352
Nicole Zheng <zznicole@hotmail.com>
2020-06-16 22:25:53 +0200

    Basic Data Structures 6-10

--- src/dataStructures.js
+++ src/dataStructures.js
@@ -31,6 +31,59 @@
 
 //5. Basic Date Structures: Remove Items Using splice()
 
-const arr = [2, 4, 5, 1, 7, 5, 2, 1];
-arr.splice(1, 4);
-console.log(arr);
+// const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// arr.splice(1, 4);
+// console.log(arr);
+
+//6. Basic Date Structures: Add Items Using splice()
+
+// function htmlColorNames(arr) {
+//   arr.splice(0, 2,'DarkSalmon', 'BlanchedAlmond');
+//   return arr;
+// }
+
+// console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+//7. Basic Date Structures: Copy Array Items Using slice()
+
+// function forecast(arr) {
+//   let newArr = arr.slice(2, 4);
+//   return newArr;
+// }
+
+// console.log(forecast(['cold','rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//8. Basic Date Structures: Copy an Array with the Spread Operator
+
+// function copyMachine(arr, num) {
+//    let newArr = [];
+//    while (num >= 1) {
+//      newArr.push([...arr]);
+//      num--;
+//    }
+//    return newArr;
+// }
+
+// console.log(copyMachine([true, false, true], 3));
+
+//9. Basic Date Structures: Combine Arrays with the Spread Operator
+
+// function spreadOut() {
+//   let fragment = ['to', 'code'];
+//   let sentence = ['learning', ...fragment, 'is', 'fun'];
+//   return sentence;
+// }
+
+// console.log(spreadOut());
+
+//10. Basic Date Structures: Check For The Presence of an Element With indexOf()
+
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'onions'));
