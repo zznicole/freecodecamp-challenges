3def5946f8ac187427c66c91365e658b5c976c91
Nicole Zheng <zznicole@hotmail.com>
2020-06-24 15:14:06 +0200

    algorithmScripting 5-10

--- src/algorithmScripting.js
+++ src/algorithmScripting.js
@@ -19,15 +19,15 @@
 
 //3.Factorialized a Number
 
-function factorialize(num) {
-  let total = 0;
-  for (let i = num; i > 0; i--) {
-    total = num[i] * num +1;
-  }
-  return total;
-}
-
-console.log(factorialize(5));
+// function factorialize(num) {
+//     for (let product = 1; num > 0; num--) {
+//       product *= num;
+//     }
+//     return product;
+//   }
+  
+//   factorialize(5);
+// console.log(factorialize(5));
 //4.Find the Longest Word in a String
 
 // function findlongestWordLength(str) {
@@ -44,11 +44,85 @@ console.log(factorialize(5));
 // console.log(findlongestWordLength("The quick brown fox jumped over the lazy dog"));
 
 //5.Return Largest Numbers in Arrays
+
+// function largestOfFour(arr) {
+  
+//   let largestNumArr = []
+  
+//   for(let i = 0; i < arr.length; i++) {
+//     let largestNum = arr[i][0];
+//     console.log(largestNum);
+//     for(let j = 1; j < arr[i].length; j++) {
+//       if (arr[i][j] > largestNum) {
+//         largestNum = arr[i][j];
+//       }
+//     }
+//   largestNumArr[i] = largestNum;
+//   }
+//   return largestNumArr;
+// }
+
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
 //6.Confirm the Ending
+
+// function confirmEnding(str, target) {
+
+//    return str.substring(str.length - target.length) === target;
+// }
+
+// console.log(confirmEnding("Bastian", "an"));
+
+
 //7.Repeat a String Repeat a String
+
+// function repeatStingNumTimes(str, num) {
+//  let repeatedStr = "";
+//  while (num > 0) {
+//   repeatedStr += str;
+//   num--;
+// }
+//     return repeatedStr;
+// }
+
+//repeatStingNumTimes("abc", -3);
+
 //8.Truncate a String
+
+// function truncateString(str, num) {
+
+// if (str.length <= num) {
+//   return str;
+// } 
+//   return str.slice(0, num) + "...";
+// }
+
+// truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
 //9.Finders Keepers
+
+// function findElement(arr, func) {
+//   let num = 0;
+//   for(let i = 0; i < arr.length; i++) {
+//     num = arr[i];
+//     if (func(num)) {
+//       return num;
+//     }
+//   }
+//   return undefined;
+// }
+
+// findElement([1, 5, 3, 4], num => num % 2 === 0 );
+
+
 //10.Boo who
+
+function booWho(bool) {
+return typeof bool === "boolean";
+}
+
+console.log(booWho(9));
 //11.Title Case a Sentence
 //12.Slice and Splice
 //13.Falsy Bouncer
